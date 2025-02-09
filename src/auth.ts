dcedf0188b8e63e4bea77ff3608d3cfa1f96de27
SAKAGAWA0401 <rts0401@gmail.com>
2025-02-09 19:02:15 +0800

    apple sign in settings

--- src/auth.ts
+++ src/auth.ts
@@ -15,7 +15,13 @@ const config: NextAuthConfig = {
         }),
         AppleProvider({
             clientId: process.env.APPLE_ID!,
-            clientSecret: process.env.APPLE_SECRET!.replace(/\\n/g, "\n"),
+            clientSecret: JSON.stringify({
+                clientId: process.env.APPLE_CLIENT_ID,
+                teamId: process.env.APPLE_TEAM_ID,      // Apple Developer Portal の Team ID
+                keyId: process.env.APPLE_KEY_ID,        // Apple Developer Portal で発行した Key ID
+                // 環境変数から取得した秘密鍵の改行を復元
+                privateKey: process.env.APPLE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+            })
         }),
         GitHubProvider({
             clientId: process.env.GITHUB_ID!,
